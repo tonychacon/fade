9f40fd54fa05bf8058077e770eda1ac97bfe1396
Scott Chacon <schacon@gmail.com>
2014-10-08 10:12:43 +0200

    properly signed off
    Signed-off-by: Scott Chacon

--- fade.ino
+++ fade.ino
@@ -3,10 +3,10 @@ int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
-void setup()  { 
+void setup()  {
   // declare pin 9 to be an output:
   pinMode(led, OUTPUT);
-} 
+}
 
 // the loop routine runs over and over again forever:
 void loop()  { 
